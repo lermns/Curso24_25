afce1879bade2e8a2b8f605fcfbfc7755d51a56b
Leonardo Ramos B. <htcmarchi@gmail.com>
2024-12-02 14:11:05 +0100

    prueba examen moviles

---
+++ Desarrollo Movil/1er/practica/prueba/app/src/main/java/com/example/prueba/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.prueba
+
+import android.os.Bundle
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val editTxt1: EditText = findViewById(R.id.editText1)
+        val editTxt2: EditText = findViewById(R.id.editText2)
+        val boton: AppCompatButton = findViewById(R.id.button)
+
+        boton.setOnClickListener{
+            editTxt2.text = editTxt1.text
+        }
+    }
+}
