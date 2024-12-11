72d08cdfc9b32bb3d43e1da4ccfd8199a55efb90
Leonardo Ramos B. <htcmarchi@gmail.com>
2024-12-11 12:25:37 +0100

    tareas de AD

--- AccesoADatos/1er/AccesoADatos/src/SqLite/ejemplos/Ejemplo_1/Main.java
+++ AccesoADatos/1er/AccesoADatos/src/SqLite/ejemplos/Ejemplo_1/Main.java
@@ -18,19 +18,23 @@ public class Main {
         }
 
         String conexionURL="jdbc:sqlite:"+nameDB;
+
         try {
             con = abreConexion(conexionURL);
+            System.out.println("En caso de que la tabla exista esta no se creara");
             System.out.println("Nombre de la tabla que quieres crear: ");
             String tableName = scStr.nextLine();
             crearTabla(con, tableName);
-/*
+            int num = datosEnTablaCount(con, tableName);
+            System.out.println(num);
+
             System.out.println("Dame el nombre del alumno: ");
             String nombre = scStr.nextLine();
 
             System.out.println("Dame el ciclo en el que esta matriculado: ");
             String ciclo = scStr.nextLine();
             insertarAlumno(con, ++contID, nombre, ciclo, tableName);
-*/
+
             mostrarRegistrosAlumnos(consultarTodosLosAlumnos(con, tableName));
 
         } catch (SQLException e) {
@@ -39,63 +43,62 @@ public class Main {
     }
 
     private static Connection abreConexion(String url) throws  SQLException{
-        System.out.println("metodo crear conexion");
+        System.out.println("1--> Metodo crear conexion");
         Connection con;
         try {
             con = DriverManager.getConnection(url);
         }catch (SQLException e){
             throw new SQLException("Problema en el metodo abreConexion");
         }
-        System.out.println("conectado con exito");
+        System.out.println("1--> conectado con exito");
         return con;
     }
 
     private static void crearTabla(Connection con, String name)throws SQLException{
-        System.out.println("metodo crear tabla");
+        System.out.println("2--> Metodo crear tabla");
         try {
             Statement ps = con.createStatement();
             ps.execute("create table if not exists " + name + "(\n" +
-                    "id int primary key,\n" +
+                    "id int AUTO_INCREMENT primary key,\n" +
                     "nombre varchar not null,\n" +
                     "ciclo varchar not null);"
             );
-            if (verificarTablaCreada(con, name)) {
-                System.out.println("tabla " + name + " creada correctamente");
-            } else {
-                System.out.println("No se pudo verificar la creación de la tabla " + name);
-            }
+
         }catch (SQLException e){
             throw new SQLException("Problema en el metodo crearTabla\n" + e.getMessage());
         }
-        System.out.println("tabla " + name + " creada correctamente");
-        contID=consultarUltID(con, name);
+        System.out.println("2--> tabla " + name + " creada correctamente");
+        if(datosEnTablaCount(con, name)>0){
+            contID=ultimoIdAlumno(con, name);
+        }
     }
 
-    private static void insertarAlumno(Connection con, int id, String name, String ciclo, String tableName) throws SQLException{
+    private static void insertarAlumno(Connection con, String name, String ciclo, String tableName) throws SQLException{
+        System.out.println("3--> Metodo insertar");
         String sqlSent  = "insert into " + tableName + " values (?,?,?);";
         PreparedStatement preStatment = con.prepareStatement(sqlSent);
-        preStatment.setInt(1, id);
-        preStatment.setString(2, name);
-        preStatment.setString(3, ciclo);
+        preStatment.setString(1, name);
+        preStatment.setString(2, ciclo);
         int filas = preStatment.executeUpdate();
         System.out.println(filas + " fila(s) afectada(s)");
+        System.out.println("3--> Metodo insertar terminado");
     }
 
     private static ResultSet consultarTodosLosAlumnos(Connection con, String tablename)throws SQLException{
         ResultSet rs;
         try{
-            System.out.println("Metodo consultar todos los alumnos");
+            System.out.println("4--> Metodo consultar todos los alumnos");
             PreparedStatement ps = con.prepareStatement("select * from " + tablename + ";");
             rs = ps.executeQuery();
         }catch (SQLException e) {
             throw new SQLException("Problema en el metodo consultarTodosLosAlumnos\n" + e.getMessage());
         }
-        System.out.println("Consulta realizada con exito");
+        System.out.println("4--> Consulta realizada con exito");
         return rs;
     }
 
     private static void mostrarRegistrosAlumnos(ResultSet resul) throws SQLException{
-        System.out.println("Metodo Mostrar Registros Alumnos");
+        System.out.println("5--> Metodo Mostrar Registros Alumnos");
         int cont=0;
         try {
             System.out.println("id\tnombre\tciclo");
@@ -113,28 +116,54 @@ public class Main {
         }catch (SQLException e){
             throw new SQLException("Problema con el metodo mostrarRegistrosAlumnos\n" + e.getMessage());
         }
-        System.out.println("consulta de datos realizada con exito.");
+        System.out.println("5--> Consulta de datos realizada con exito.");
     }
 
-    private static int consultarUltID(Connection con, String tablename)throws SQLException{
+    private static int ultimoIdAlumno(Connection con, String tablename)throws SQLException{
         ResultSet rs;
         int idValue;
         try{
-            System.out.println("Metodo consultar todos los alumnos");
+            System.out.println("6--> Metodo consultar todos los alumnos");
             PreparedStatement ps = con.prepareStatement("select max(id) from " + tablename + ";");
             rs = ps.executeQuery();
-            idValue = rs.getInt("id");
+            idValue = rs.getInt(1);
         }catch (SQLException e){
             throw new SQLException("Problema con el metodo consultarUltID");
         }
-        System.out.println("Metodo de idValue terminado");
+        System.out.println("6--> Metodo de idValue terminado");
         return idValue;
     }
-    private static boolean verificarTablaCreada(Connection con, String name) throws SQLException {
-        DatabaseMetaData meta = con.getMetaData();
-        try (ResultSet rs = meta.getTables(null, null, name, new String[]{"TABLE"})) {
-            return rs.next(); // Si hay un resultado, la tabla existe
+
+    private static int datosEnTablaCount(Connection con, String tablename)throws SQLException{
+        ResultSet rs;
+        int idValue;
+        try{
+            System.out.println("7--> Metodo datosEnTablaCount cantidad alumnos");
+            PreparedStatement ps = con.prepareStatement("select count(*) from " + tablename + ";");
+            rs = ps.executeQuery();
+            idValue = rs.getInt(1);
+        }catch (SQLException e){
+            throw new SQLException("Problema con el metodo datosEnTablaCount");
         }
+        System.out.println("7--> Metodo de datosEnTablaCount terminado");
+        return idValue;
+    }
+
+    private static void eliminarAlumnoPorId(Connection con, String tablename)throws SQLException{
+        System.out.println("8 --> Metodo eliminar Alumnos Por ID");
+        int idValue;
+        System.out.println("Dame el id del alumno que quieras borrar: ");
+        int id = new Scanner(System.in).nextInt();
+        try{
+            System.out.println("7--> Metodo datosEnTablaCount cantidad alumnos");
+            PreparedStatement ps = con.prepareStatement("delete from ? where id=?;");
+            ps.setString(1, tablename);
+            ps.setInt(2, id);
+            idValue = ps.executeUpdate();
+        }catch (SQLException e){
+            throw new SQLException("Problema con el metodo datosEnTablaCount");
+        }
+        System.out.println("8 --> Metodo eliminar Alumnos Por ID");
     }
 }
 /*
