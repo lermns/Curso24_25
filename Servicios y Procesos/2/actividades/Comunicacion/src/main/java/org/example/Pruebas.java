6fd10e17fd60c4929b1b92427ad0e76d03f716a4
Leonardo Ramos B. <htcmarchi@gmail.com>
2025-02-21 14:01:44 +0100

    PSP actividades clase

--- Servicios y Procesos/2/actividades/Comunicacion/src/main/java/org/example/Pruebas.java
+++ Servicios y Procesos/2/actividades/Comunicacion/src/main/java/org/example/Pruebas.java
@@ -1,10 +1,35 @@
 package org.example;
 
-import java.net.*;
-
 public class Pruebas {
-    public static void main(String[] args) throws SocketException, UnknownHostException {
-        String s = "C1:C2:Mensaje";
-        System.out.println(s.split(":",2)[1]);
+    public static void main(String[] args){
+        double num = 123456789;
+        int cont=0;
+        int cont_mantisa = 0;
+        double numDiv = num/10;
+        System.out.println(numDiv);
+
+        double num_two = Math.pow(numDiv, 10);
+
+        System.out.println(num_two);
+
+        num_two = Math.pow(num_two, 10);
+
+        System.out.println(num_two);
+
+        while (num_two>10){
+            num_two/=10;
+            cont_mantisa++;
+        }
+
+        while (num>10){
+            num/=10;
+            cont++;
+        }
+
+        System.out.println(num_two + " -> mantisa " + cont_mantisa);
+
+        System.out.println(num + " -> cant " + cont);
+
+        //System.out.println(Math.log10(num)); 8,0
     }
 }
